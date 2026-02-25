05ecf67b48c222a27065eaacfc3ab6874ec01218
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2026-02-24 21:47:06 -0300

    feat: Adicionando a rota "/protocolos/ID" e o método POST

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
 import http from "http"
 
 const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === "/health"){
-        res.writeHead(201, {"Content-Type":"application/json"})
-        res.end(JSON.stringify({ status: "OK" }))
-        return
-    }
-    if (req.method === "GET" && req.url === "/protocolos"){
-        res.writeHead(200, {"Content-Type":"application/json"})
-        res.end(JSON.stringify())
-        return
-    }
+        if (req.method === "GET" && req.url === "/health") {
+                res.writeHead(201, { "Content-Type": "application/json" })
+                res.end(JSON.stringify({ status: "OK" }))
+                return
+        }
+        if (req.method === "GET" && req.url === "/protocolos") {
+                res.writeHead(200, { "Content-Type": "application/json" })
+                res.end(JSON.stringify())
+                return
+        }
+        if (req.method === "GET" && req.url === "/protocolos/ID"){
+
+        }
+        if (req.method === "POST" && req.url === "/protocolos"){
+
+        }
+        
+        req.writeHead(404)
+        req.end()
 })
 
 server.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
+        console.log("Servidor rodando na porta 3000")
 })
