5bbe630a14ca3c1aef07b9bb706b334e57664333
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2026-02-24 21:27:03 -0300

    feat: Adicionando rotas "/heatlh" e "/protocolos"

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-//Código do servidor
+import http from "http"
+
+const server = http.createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/health"){
+        res.writeHead(201, {"Content-Type":"application/json"})
+        res.end(JSON.stringify({ status: "OK" }))
+        return
+    }
+    if (req.method === "GET" && req.url === "/protocolos"){
+        res.writeHead(200, {"Content-Type":"application/json"})
+        res.end(JSON.stringify())
+        return
+    }
+})
+
+server.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
